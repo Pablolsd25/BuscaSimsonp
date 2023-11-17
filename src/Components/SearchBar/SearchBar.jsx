4c01db47b9c612ecd734bbebd03de56e162b17ea
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-11-16 22:17:00 -0600

    SearchBar//Muestra resultados//Pendient//Corregir Renderizado

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,26 +1,65 @@
 import { useState } from "react";
 import Search from "../../assets/Search.png";
+import { CardCharacter } from "../CardCharacter/CardCharacter"; // Import the CardCharacter component
 import "./SearchBar.css";
-
-const SearchBar = ({ onSearch }) => {
+const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const [searchResult, setSearchResult] = useState(null);
+  const [searchResults, setSearchResults] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+
+  const apiUrl = "https://apisimpsons.fly.dev/api/personajes";
+
+  const fetchData = async () => {
+    try {
+      const searchUrl = searchQuery
+        ? `${apiUrl}/find/${searchQuery}?limit=5&page=1`
+        : `${apiUrl}?limit=5&page=1`;
+
+      const response = await fetch(searchUrl);
+
+      if (!response.ok) {
+        throw new Error(`Error HTTP! Estado: ${response.status}`);
+      }
+
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        const data = await response.json();
+        setSearchResults(data.result || []);
+      } else {
+        console.error(
+          "Tipo de contenido inválido en la respuesta:",
+          contentType
+        );
+      }
+    } catch (error) {
+      console.error("Error al obtener los datos:", error);
+      setSearchResults([]);
+    }
+    setShowResults(true);
+  };
 
   const handleSearch = async () => {
     if (searchQuery.trim() !== "") {
       try {
-        const result = await onSearch(searchQuery);
-        setSearchResult(result);
+        await fetchData();
       } catch (error) {
         console.error("Error en la búsqueda:", error);
-        setSearchResult(null);
       }
     }
   };
 
   return (
     <div className="nav-bar">
-      <img className="search" alt="Search" src={Search} />
+      <img className="search" alt="Buscar" src={Search} />
+      <div className={`card-container ${showResults ? "" : "hidden"}`}>
+        {searchResults.map((character) => (
+          <CardCharacter
+            key={character._id}
+            name={character.Nombre}
+            image={character.Imagen}
+          />
+        ))}
+      </div>
       <div className="overlap-group">
         <input
           type="text"
@@ -36,13 +75,6 @@ const SearchBar = ({ onSearch }) => {
         />
         <button onClick={handleSearch}>Buscar</button>
       </div>
-
-      {searchResult && (
-        <div className="search-result">
-          <p>{searchResult.name}</p>
-          <img src={searchResult.image} alt={searchResult.name} />
-        </div>
-      )}
     </div>
   );
 };
